a249cb9f97424d78f0e79674457e6c7bffd503a9
Yusi Yuan <yuanyusi2005@gmail.com>
2025-08-22 18:41:41 +0800

    add ToDos, Deadlines, Events and their corresponding functionality

--- src/main/java/Sloth.java
+++ src/main/java/Sloth.java
@@ -1,6 +1,9 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Sloth {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String logo =
                 "   🌿─────      \n" +
                         "  ( - . - )    \n" +
@@ -25,32 +28,53 @@ public class Sloth {
                 System.out.println(line);
                 for (int i = 1; i <= lst.size(); i++) {
                     Task t = lst.get(i - 1);
-                    String s = t.getContent();
-                    System.out.println("\t" + i+ ". " + "[" + t.getStatus() + "] "+ s);
+                    System.out.println("\t" + i+ ". " + t);
                 }
                 System.out.println(line);
             } else {
                 String[] split = input.split(" ");
                 if (!(split[0].equals("mark") || split[0].equals("unmark"))) {  // add something.
-                    lst.add(new  Task(input));
                     System.out.println(line);
-                    System.out.println("added: " + input);
+                    Task task_to_add = new Task("");
+                    if (split[0].toLowerCase().equals("todo")) {  /* ToDo */
+                        Pattern todo_pat = Pattern.compile("^todo\\s(.+)$");
+                        Matcher match =  todo_pat.matcher(input);
+                        match.matches();
+                        String content = match.group(1);
+                        lst.add(task_to_add = new ToDo(content));
+
+                    } else if (split[0].toLowerCase().equals("deadline")) {  /* Deadline */
+                        Pattern deadline_pat = Pattern.compile("^deadline\\s+(.+)\\s/by\\s(.+)$");
+                        Matcher match = deadline_pat.matcher(input);
+                        match.matches();
+                        String content = match.group(1), endDate = match.group(2);
+                        lst.add(task_to_add = new Deadline(content, endDate));
+                    } else {  /* Event */
+                        Pattern event_pat = Pattern.compile("^event\\s+(.+)\\s/from\\s(.+)\\s/to\\s(.+)$");
+                        Matcher match = event_pat.matcher(input);
+                        match.matches();
+                        String content = match.group(1), startDate = match.group(2), endDate = match.group(3);
+                        lst.add(task_to_add = new Event(content, startDate, endDate));
+                    }
+                    System.out.println("okayy ... I've added " + task_to_add);
+                    System.out.println("Let... me.... calculate...");
+                    Thread.sleep(2000);
+                    System.out.println("Now you have " + lst.size() + " tasks \uD83E\uDDA5");
                     System.out.println(line);
                 } else {  // mark or unmark
                     int idx = Integer.parseInt(split[1]);
-                    Task event = lst.get(idx-1);
+                    Task stuff = lst.get(idx-1);
                     System.out.println(line);
                     if (split[0].equals("mark")) {
-                        if (event.getStatus().equals(" "))  event.toggleStatus();
+                        if (stuff.getStatus().equals(" "))  stuff.toggleStatus();
                         System.out.println("Well... done! I've marked the following as done:");
                     } else {
-                        if (event.getStatus().equals("X")) event.toggleStatus();
+                        if (stuff.getStatus().equals("X")) stuff.toggleStatus();
                         System.out.println("okay... I've marked the following as undone:");
                     }
-                    System.out.println("\t" + "[" + event.getStatus() + "]" + " " + event.getContent());
+                    System.out.println(stuff);
                     System.out.println(line);
                 }
-
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,56 @@ public class Task {
     public void toggleStatus() {
         this.done = !this.done;
     }
+
+    @Override
+    public String toString() {
+        return "[" + getStatus() + "] " + getContent();
+    }
 }
+
+class ToDo extends Task {
+    private final String type = "T";
+
+    public ToDo(String content) {
+        super(content);
+    }
+    @Override
+    public String toString() {
+        return "[" + this.type + "] [" + this.getStatus() + "] " + this.getContent();
+    }
+}
+
+class Deadline extends Task {
+    private final String type = "D";
+    private String endDate;
+
+    public Deadline(String content,  String endDate) {
+        super(content);
+        this.endDate = endDate;
+    }
+    @Override
+    public String toString() {
+        return "[" + this.type + "] [" + this.getStatus() + "] " + this.getContent()
+                + " (by: " + this.endDate + ")";
+    }
+}
+
+class Event extends Task {
+    private final String type = "E";
+    private String endDate;
+    private String startDate;
+
+    public Event(String content, String startDate, String endDate) {
+        super(content);
+        this.endDate = endDate;
+        this.startDate = startDate;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.type + "] [" + this.getStatus() + "] " + this.getContent()
+                + " (from: " + this.startDate + " to: " + this.endDate + ")";
+    }
+}
+
+
