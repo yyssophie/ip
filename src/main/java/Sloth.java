552b3b57a931f6796169efd749790ee002241a3c
Yusi Yuan <yuanyusi2005@gmail.com>
2025-08-22 10:58:35 +0800

    add echo functionality

--- src/main/java/Sloth.java
+++ src/main/java/Sloth.java
@@ -1,14 +1,30 @@
+import java.util.*;
 public class Sloth {
     public static void main(String[] args) {
         String logo =
-                "   z z Z       \n" +
-                        "  (  -.-)    \n" +
-                        "  (   -   )    \n" +
-                        "   ‾‾‾‾‾‾‾     \n";
+                "   🌿─────      \n" +
+                        "  ( - . - )    \n" +
+                        "  /(   づ )    \n" +
+                        "  ╯ ╯ ╯ ╯     \n";
         String line = "_".repeat(50);
+        /* Greetings */
         System.out.println(line);
-        System.out.println("Hello, I am ... Sloth\n" + logo + "\nHow can I help you today?");
+        System.out.println("Hello, I am ... Sloth\n" + logo + "\n" + "How can I help you today?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you soon~\nI'm gonna go back to sleep  zZZ");
+        /* Echo */
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String input = sc.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you soon~\nI'm gonna...go back to...sleep  zZZ");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
     }
 }
