a9af4399c3c4c780e8266c8d34320e9a34ce937b
Yusi Yuan <yuanyusi2005@gmail.com>
2025-08-22 10:43:23 +0800

    rename, greet and exit

---
+++ src/main/java/Sloth.java
@@ -0,0 +1,14 @@
+public class Sloth {
+    public static void main(String[] args) {
+        String logo =
+                "   z z Z       \n" +
+                        "  (  -.-)    \n" +
+                        "  (   -   )    \n" +
+                        "   ‾‾‾‾‾‾‾     \n";
+        String line = "_".repeat(50);
+        System.out.println(line);
+        System.out.println("Hello, I am ... Sloth\n" + logo + "\nHow can I help you today?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you soon~\nI'm gonna go back to sleep  zZZ");
+    }
+}
