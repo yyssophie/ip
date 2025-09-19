34793f5038f0f25b68b57a71366c2a6fdf7223d7
Yusi Yuan <yuanyusi2005@gmail.com>
2025-09-19 23:22:10 +0800

    Add window title

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -27,6 +27,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            stage.setTitle("Sloth");
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
